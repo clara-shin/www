74edb60d9eef809b83260a57d80f87072f416d8a
Hyejin Shin <enthusiastic.shj@gmail.com>
2017-11-17 17:03:24 +0900

    로그인 폼 관련 영역 일부 마크업완성

--- js/menu-tab.js
+++ js/menu-tab.js
@@ -1,16 +1,42 @@
+// $(document).ready(function(){ //문서준비 이벤트
+// 	var menu = $('.main-menu > li');
+// 	var subMenu = $('.sub-menu');
+// 	var last = $('.sub-menu li:last-child a');
+	
+// 	//어느 메뉴위에 마우스오버가 되면 그 해당메뉴의 서브메뉴
+// 	menu.hover(function(){
+// 		$(this).find(subMenu).toggleClass('menu-act');
+// 	});
+// 	menu.focusin(function(){
+// 		$(this).find(subMenu).addClass('menu-act');
+// 	});
+// 	last.focusout(function(){
+// 		$(this).parents('.sub-menu').removeClass('menu-act');
+// 	});
+// }); //jquery end
+
+
+
+// $()
+// document 
+
 $(document).ready(function(){
 	var menu = $('.main-menu > li');
-	var subMenu = $('.sub-menu');
-	var last = $('.sub-menu li:last-child a');
-	
-	//어느 메뉴위에 마우스오버가 되면 그 해당메뉴의 서브메뉴
+
 	menu.hover(function(){
-		$(this).find(subMenu).toggleClass('menu-act');
+		$(this).find('.sub-menu').toggleClass('menu-act');
 	});
+
 	menu.focusin(function(){
-		$(this).find(subMenu).addClass('menu-act');
+		$(this).siblings().find('.sub-menu').removeClass('menu-act'); //reset
+		$(this).find('.sub-menu').addClass('menu-act');
 	});
-	last.focusout(function(){
-		$(this).parents('.sub-menu').removeClass('menu-act');
-	});
-}); //jquery end
+
+	// $('.box').on('click', function(){
+	// 	$(this).toggleClass('box-act');
+	// });
+
+	
+});//jQuery end
+
+//hover는 .on()에 바인딩할수없음, 단일이벤트라서
